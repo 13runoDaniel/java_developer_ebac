04d397c1fdd1ba5a6f79cb95b0c6ccff6909a671
Bruno Daniel <bdao.profissional@gmail.com>
2025-03-05 21:08:58 -0300

    adicionando metodos a classes em Java

--- PrimeiraClasse/src/PrimeiraClasse.java
+++ PrimeiraClasse/src/PrimeiraClasse.java
@@ -5,5 +5,16 @@ public class PrimeiraClasse {
         System.out.println("Olá Bruno Daniel");
 
         Cliente cliente = new Cliente();
+
+        cliente.cadastrarEndereco("São Paulo - SP, Brasil");
+        cliente.setCodigo(1);
+        cliente.imprimirEndereco();
+        String end = cliente.retornarNomeCliente();
+        int valor = cliente.getValorTotal();
+
+        System.out.println(cliente.getCodigo());
+        System.out.println(cliente.getEndereco());
+        System.out.println(end);
+        System.out.println(valor);
     }
 }
