bbca992eca62542f11c545bb68ee80b831ebbc75
Bruno Daniel <bdao.profissional@gmail.com>
2025-03-05 22:35:22 -0300

    tarefa programa calculo de media

---
+++ TarefaCalculoMedia/src/Media.java
@@ -0,0 +1,18 @@
+public class Media {
+
+    public static void main(String[] args) {
+
+        calcularMedia();
+    }
+
+    public static void calcularMedia() {
+
+        float mediaA = 9f;
+        float mediaB = 7f;
+        float mediaC = 3f;
+        float mediaD = 6f;
+
+        float resultado = (mediaA +  mediaB + mediaC + mediaD) / 4;
+        System.out.println(resultado);
+    }
+}
